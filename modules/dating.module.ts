a42c4a19b8bf9f41eed32d61c6c9d4e5d56376ea
Niccolò Olivieri Achille <n.olivieriachille@gmail.com>
2022-08-29 00:42:00 +0200

    fix(dating): :bug: Fixed the dating step claiming and starting

--- modules/dating.module.ts
+++ modules/dating.module.ts
@@ -1,3 +1,4 @@
+import environmentConfig from '../configs/environment';
 import { DatingStepInterface } from '../interfaces/game.interface';
 import bigBangEmpire from '../libs/big-bang-empire';
 import request from '../libs/request';
@@ -36,19 +37,41 @@ class DatingStep {
 
 class DatingModule {
   async checkStep() {
-    const datingSteps = bigBangEmpire.game.dating_step.map((datingStep) => new DatingStep(datingStep));
+    const datingSteps = bigBangEmpire.game.dating_step.map(
+      (datingStep) => new DatingStep(datingStep),
+    );
 
-    if (datingSteps.some((datingStep) => datingStep.status !== DatingStepStatus.Started)) {
+    await datingSteps
+      .filter((datingStep) => datingStep.status === DatingStepStatus.Finished)
+      .reduce(async (promise, datingStep) => {
+        await promise;
+
+        await this.claimDatingStepReward(datingStep);
+
+        const dating = environmentConfig.game.constants.dating[datingStep.datingIndex];
+
+        if (dating.steps[datingStep.stepIndex + 1]) {
+          await this.startNewDatingStep(datingStep);
+        }
+      }, Promise.resolve());
+
+    if (datingSteps.some((datingStep) => datingStep.status !== DatingStepStatus.Finished)) {
       console.log('Dating finished!!!');
-
-      await this.startNewDatingStep(datingSteps[0]);
     }
+  }
+
+  async claimDatingStepReward(datingStep: DatingStep) {
+    await request.post('claimDatingStepReward', {
+      dating_index: datingStep.datingIndex.toString(),
+      step_index: datingStep.stepIndex.toString(),
+      discard_item: 'false',
+    });
   }
 
   async startNewDatingStep(datingStep: DatingStep) {
     await request.post('startNewDatingStep', {
       dating_index: datingStep.datingIndex.toString(),
-      step_index: datingStep.stepIndex.toString(),
+      step_index: (datingStep.stepIndex + 1).toString(),
     });
   }
 }
