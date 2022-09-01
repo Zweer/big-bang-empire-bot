6531503e31f1054c8b4e7380943d1fa39745a77a
Niccolò Olivieri Achille <n.olivieriachille@gmail.com>
2022-09-01 18:17:18 +0200

    feat(telegram): :sparkles: Added the character section to the bot

--- libs/telegram.ts
+++ libs/telegram.ts
@@ -1,7 +1,10 @@
 import numbro from 'numbro';
 import { Telegraf } from 'telegraf';
 
+import environment from '../configs/environment';
 import characterModule from '../modules/character/character.module';
+import gameModule from '../modules/game/game.module';
+import inventoryModule from '../modules/inventory/inventory.module';
 import movieModule from '../modules/movie/movie.module';
 import profileModule from '../modules/profile/profile.module';
 import { LeaderboardSortType } from '../modules/profile/types';
@@ -15,6 +18,7 @@ class Telegram {
 
     this.initHelp();
     this.initProfile();
+    this.initCharacter();
 
     this.telegraf.launch();
 
@@ -32,10 +36,9 @@ class Telegram {
       context.reply(`From here you can control your Big Bang Empire Bot.
 Commands are:
 /help - View this help message
-/profile - Print info about your hero
-/restart - Restarts the game (if something bad happened)
-/close - Shout down the game (enough?)
-/closeWhenNoEnergy - Set to shut down when no energy`),
+/profile - Print info about your account
+/character - Print info about your hero
+`),
     );
   }
 
@@ -51,7 +54,7 @@ Commands are:
       const fansLeaderboard = await profileModule.getLeaderboard(LeaderboardSortType.Fans);
 
       const msg = [];
-      msg.push(`Lexma`);
+      msg.push(character.name);
       msg.push(
         `- lvl ${character.level} (${numbro(character.levelPercentage).format({
           output: 'percent',
@@ -65,6 +68,9 @@ Commands are:
         })}`,
       );
       msg.push(`- ${numbro(user.premiumCurrency).format({ average: true })} gems`);
+      msg.push(
+        `- ${gameModule.game.current_optical_changes.resource} / ${environment.game.constants.optical_change_chest_price_small} optical`,
+      );
       msg.push(
         `- ${numbro(character.honor).format({ average: true })} honor (${honorLeaderboard.rank} / ${
           honorLeaderboard.total
@@ -95,6 +101,24 @@ Commands are:
         );
         msg.push(`- movie energy: ${character.movieEnergy}`);
       }
+
+      context.reply(msg.join('\n'));
+    });
+  }
+
+  initCharacter() {
+    this.telegraf.command('character', (context) => {
+      const character = characterModule.character;
+      const inventory = inventoryModule.inventory;
+
+      const msg: string[] = [];
+      msg.push(character.name);
+      msg.push(`- Strength: ${character.statTotalStrength}`);
+      msg.push(`- Stamina: ${character.statTotalStamina}`);
+      msg.push(`- Dodge: ${character.statTotalDodgeRating}`);
+      msg.push(`- Critical: ${character.statTotalCriticalRating}`);
+      msg.push(`- Weapon: ${character.statWeaponDamage}`);
+      msg.push(`- Missiles: ${inventory.missilesItem?.charges ?? 0}`);
 
       context.reply(msg.join('\n'));
     });
